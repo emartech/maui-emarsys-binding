c62eb5ae495793d6bc8dd7aa9c48bdfbb493c5a4
biancalui-emarsys <bianca.lui@emarsys.com>
2024-11-26 18:30:30 +0800

    feat: NotificationServiceExtension
    MV-851

--- sample/Platforms/Android/Services/MyFirebaseMessagingService.cs
+++ sample/Platforms/Android/Services/MyFirebaseMessagingService.cs
@@ -1,4 +1,8 @@
-// This is a sample FirebaseMessagingService subclass that demostrates
+// It is recommended to use EmarsysFirebaseMessagingService to automatcially handle push.
+// Please register the service in your manifest.
+// Reference to sample: https://github.com/emartech/maui-emarsys-binding/blob/main/sample/Platforms/Android/AndroidManifest.xml#L4
+
+// If it is needed, this is a sample FirebaseMessagingService subclass that demostrates
 // how push token and received push message should be handled by Emarsys SDK in a custom implementation.
 // Reference to Native SDK sample: https://github.com/emartech/android-emarsys-sdk?tab=readme-ov-file#java
 
